6d17485386db0b80e8290d58501167f0921c3365
mj <xnfxnf7979@daum.net>
2020-09-28 12:28:39 +0900

    12:28

---
+++ README/Readme01.py
@@ -0,0 +1,14 @@
+"""
+MVC 패턴
+Model: DTO (data transfer object) + DAO (data access object)
+Service: Business Login (Algorithm)
+Controller: RESTful 방식으로 React Axios 로 통신
+AI-Calc 에서 사칙연산을 나누는 이유
+머신 model-* 은 history 를 기억하는 능력이 있습니다.
+5 + 2 = 7 
+5 + * = *
+....
+5 덧셈연산을 제안 ...
+7 * ....
+7 곱셈연산을 제안 ...
+"""
